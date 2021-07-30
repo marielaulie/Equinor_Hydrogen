4cbc386d4536709ffb0dbdb8157ceb0d300de318
marielaulie <54407324+marielaulie@users.noreply.github.com>
2021-07-30 11:32:14 +0200

    Opprettet app

---
+++ Equinor_Hydrogen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Equinor_Hydrogen
+//
+//  Created by Mariel Aulie Hinderaker on 30/07/2021.
+//  Copyright © 2021 Mariel Aulie Hinderaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
